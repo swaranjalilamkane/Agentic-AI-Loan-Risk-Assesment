ac4fae3d50ba60c6b513125b9818735f5e01c6d1
ashisubramaniyan14 <ashasubramaniyan2002@gmail.com>
2026-03-09 20:45:19 -0400

    Implement PlaidConnector class for transaction retrieval

--- src/data_integration/plaid_connector.py
+++ src/data_integration/plaid_connector.py
@@ -1,1 +1,17 @@
+from plaid import Client
 
+class PlaidConnector:
+    def __init__(self, client_id, secret):
+        self.client = Client(
+            client_id=client_id,
+            secret=secret,
+            environment='sandbox'
+        )
+
+    def get_transactions(self, access_token):
+        response = self.client.Transactions.get(
+            access_token,
+            start_date="2023-01-01",
+            end_date="2023-12-31"
+        )
+        return response["transactions"]
